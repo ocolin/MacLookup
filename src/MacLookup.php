23f84b4f4e45cb75e1e5d9101f5f979f3f621fbe
Colin Miller <ocolin@cruzio.com>
2025-10-08 12:12:50 -0400

    Fixed bug with private addresses

--- src/MacLookup.php
+++ src/MacLookup.php
@@ -147,7 +147,7 @@ class MacLookup
     public static function is_Private( string $mac ) : bool
     {
         $private = [ '2', '6', 'A', 'E' ];
-        $check_char = substr( string: $mac, offset: 0, length: 1 );
+        $check_char = substr( string: $mac, offset: 1, length: 1 );
 
         return in_array( needle: $check_char, haystack: $private );
     }
